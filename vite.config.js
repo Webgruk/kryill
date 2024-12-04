461bef6256f1e83324a04edb9bfd186318270cf6
immanuel <immanuelajimah@gmail.com>
2024-12-04 19:03:38 +0100

    completed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://form-data-dhwk.onrender.com',
+      '/api': 'https://form-data-dhwk.onrender.com/api',
     },
   },
   build: {
